1811d89dd6f4990dfdbad4982ecf42c857352d5b
okzkx <okokzkxok@126.com>
2020-02-13 13:03:34 +0800

    Transfer CSharpFormator

---
+++ CSharpFormator/Samples/DoubleDimensional.cs
@@ -0,0 +1,28 @@
+﻿/**********************************************************************
+* 
+***********************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace OKZXK.Util.CSharpFormator
+{
+    public class DoubleDimensional
+    {
+        public static int[][] Get()
+        {
+            int[][] vss = new int[5][];
+            for (int i = 0; i < 5; i++)
+            {
+                vss[i] = new int[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    vss[i][j] = i * j;
+                }
+            }
+            return vss;
+        }
+
+      
+    }
+}
---
+++ CSharpFormator/Samples/ListTable.cs
@@ -0,0 +1,44 @@
+﻿/**********************************************************************
+* 
+***********************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace OKZXK.Util.CSharpFormator
+{
+    public class ListTable
+    {
+        static void Main(string[] args)
+        {
+            var vss = DoubleDimensional.Get();
+
+            FormaterSequence formaterSequence = new FormaterSequence();
+            DataFormater dataFm = null;
+            EnumerableFormater colFm = null;
+            EnumerableFormater rowFm = null;
+
+            // List Table
+            dataFm = new DataFormater((data) => string.Format("{0,5}", data));
+            colFm = new EnumerableFormater((i, data) => dataFm.Format(i + 1));
+            formaterSequence.Add(colFm, vss[0]);
+
+            colFm = new EnumerableFormater(dataFm);
+            rowFm = new EnumerableFormater(colFm);
+            rowFm.separator = "\n";
+
+            formaterSequence.Add(rowFm, vss);
+            formaterSequence.Show();
+
+            //Dictionary Table
+            Dictionary<string, int[]> dict = new Dictionary<string, int[]>();
+            for (int i = 0; i < vss.Length; i++)
+            {
+                dict.Add(i.ToString(), vss[i]);
+            }
+            PairFormater<string, int[]> pairFormater = new PairFormater<string, int[]>(colFm);
+            DictionaryFormater<string, int[]> dictionaryFormater = new DictionaryFormater<string, int[]>(pairFormater);
+            dictionaryFormater.Show(dict);
+        }
+    }
+}
